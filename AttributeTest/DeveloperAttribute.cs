64ddffa9d61d9ade549df9f5ead0e290a2b4ec35
witness7547 <754716762@qq.com>
2018-03-30 14:08:36 +0800

    AttributeTest

---
+++ AttributeTest/DeveloperAttribute.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeTest
+{
+    [System.AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+    sealed class MyAttribute : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string positionalString;
+
+        // This is a positional argument
+        public MyAttribute(string positionalString)
+        {
+            this.positionalString = positionalString;
+
+            // TODO: Implement code here
+
+            throw new NotImplementedException();
+        }
+
+        public string PositionalString
+        {
+            get { return positionalString; }
+        }
+
+        // This is a named argument
+        public int NamedInt { get; set; }
+    }
+
+
+    [AttributeUsage(AttributeTargets.All)]
+    public class DeveloperAttribute : Attribute
+    {
+        // Private fields.
+        private string name;
+        private string level;
+        private bool reviewed;
+
+        // This constructor defines two required parameters: name and level.
+
+        public DeveloperAttribute(string name, string level)
+        {
+            this.name = name;
+            this.level = level;
+            this.reviewed = false;
+        }
+
+        // Define Name property.
+        // This is a read-only attribute.
+
+        public virtual string Name
+        {
+            get { return name; }
+        }
+
+        // Define Level property.
+        // This is a read-only attribute.
+
+        public virtual string Level
+        {
+            get { return level; }
+        }
+
+        // Define Reviewed property.
+        // This is a read/write attribute.
+
+        public virtual bool Reviewed
+        {
+            get { return reviewed; }
+            set { reviewed = value; }
+        }
+    }
+}
---
+++ AttributeTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+//[assembly: AssemblyTitle("My Assembly")]
+
+namespace AttributeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Example.Add(1, 2));
+        }
+    }
+
+    public class Example
+    {
+        // Specify attributes between square brackets in C#.
+        // This attribute is applied only to the Add method.
+        [Obsolete("Will be removed in next version."/*, true*/)]
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
