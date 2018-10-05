d98e179111ec8d70152266284411051d2a82e6b1
witness7547 <754716762@qq.com>
2018-10-05 16:07:40 +0800

    mutex test

---
+++ SynchronizationPrimitives/MutexTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronizationPrimitives
+{
+    class MutexTest
+    {
+        static void Main(string[] args)
+        {
+            bool createdNew;
+            Mutex mutex = new Mutex(false, "MyMutex", out createdNew);
+
+            if (!createdNew)
+            {
+                Console.WriteLine("You can only start one instance of the Application");
+                return;
+            }
+
+            Console.WriteLine("This is MutexTest App!");
+            Console.ReadLine();
+        }
+    }
+}
